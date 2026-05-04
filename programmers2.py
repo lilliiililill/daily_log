571d90d34e32cd4ffeecb0aaa91bfe28e4464b65
lilliiililill <asder0418@gmail.com>
2026-05-04 10:24:31 +0900

    Day 1

---
+++ programmers2.py
@@ -0,0 +1,8 @@
+# 2026.05.04
+# programmers2.py
+
+def solution(num1, num2):
+
+    answer = num1 - num2
+
+    return answer
---
+++ programmers4.py
@@ -0,0 +1,8 @@
+# 2026.05.04
+# programmers4.py
+
+def solution(num1, num2):
+
+    answer = num1 // num2
+
+    return answer
