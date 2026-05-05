11e8201ed109b5182a21c78921afad1830b3556e
lilliiililill <asder0418@gmail.com>
2026-05-05 09:06:25 +0900

    Add files via upload

---
+++ programmers6.py
@@ -0,0 +1,6 @@
+# 2026.05.05
+# programmers6.py
+
+def solution(num1, num2):
+    answer = 1 if num1 == num2 else -1
+    return answer
---
+++ programmers8.py
@@ -0,0 +1,12 @@
+# 2026.05.05
+# programmers8.py
+
+def solution(numbers):
+    answer = []
+    
+    for i in numbers:
+        
+        a = i*2
+        answer.append(a)
+        
+    return answer
