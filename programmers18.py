d6e4d93b676924e0660ae9cdb8937494baf1e8c2
lilliiililill <asder0418@gmail.com>
2026-05-08 08:59:06 +0900

    Day 5

---
+++ programmers18.py
@@ -0,0 +1,8 @@
+# 2026.05.08
+# programmers18.py
+
+def solution(age):
+    
+    answer = 2023 - age
+    
+    return answer
---
+++ programmers19.py
@@ -0,0 +1,12 @@
+# 2026.05.08
+# programmers19.py
+
+def solution(num_list):
+    
+    answer = []
+    
+    for i in reversed(num_list):
+        
+        answer.append(i)
+        
+    return answer
