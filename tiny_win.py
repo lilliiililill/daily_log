c5821055b9a0e1da52fa0f83ab502f77dc959564
lilliiililill <asder0418@gmail.com>
2026-05-17 14:22:41 +0900

    tiny_win.py

---
+++ tiny_win.py
@@ -0,0 +1,17 @@
+# 2026.05.17
+# tiny_win.py
+
+messages = [
+
+    "오늘도 한 줄 전진 완료",
+    "컴파일보다 중요한 건 꾸준함",
+    "작게 끝내도 끝낸 건 끝낸 거다",
+
+    ]
+
+for msg in messages:
+
+    input("Enter > ")
+    print(msg)
+
+print("\n오늘 코딩 종료.")
