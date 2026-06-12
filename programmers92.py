d6f4e31893f54c8538fc1bd36c2f353b02891ff2
lilliiililill <asder0418@gmail.com>
2026-06-12 09:35:29 +0900

    Day 23 (4)

---
+++ programmers92.py
@@ -0,0 +1,22 @@
+# 2026.06.12
+# programmers92.py
+
+def solution(id_pw, db):
+    
+    answer = ''
+    
+    for i in db:
+        
+        if i[0] == id_pw[0] and i[1] == id_pw[1]:
+            
+            answer = 'login'
+            
+        elif i[0] == id_pw[0] and i[1] != id_pw[1]:
+            
+            answer = "wrong pw"
+            
+        elif i[0] != id_pw[0] and i[1] != id_pw[1]:
+            
+            answer = "fail"
+    
+    return answer
