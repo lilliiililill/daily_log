9c054b2e41c0e70a7d4466bf8b9640d306c3581d
lilliiililill <asder0418@gmail.com>
2026-07-01 00:32:55 +0900

    Day 27 (1~5)

---
+++ programmers106.py
@@ -0,0 +1,5 @@
+# 2026.07.01
+# programmers106.py
+
+a, b = map(int, input().strip().split(' '))
+print(a, "+" ,b, "=", a+b)
---
+++ programmers107.py
@@ -0,0 +1,6 @@
+# 2026.07.01
+# programmers107.py
+
+str1, str2 = input().strip().split(' ')
+
+print(str1+str2)
