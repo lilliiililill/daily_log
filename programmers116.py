ab2bf9bd804756a0644c3a2a8adc69797b41085d
lilliiililill <asder0418@gmail.com>
2026-07-06 22:22:44 +0900

    Day 29 (1~3)

---
+++ programmers116.py
@@ -0,0 +1,15 @@
+# 2026.07.06
+# programmers116.py
+
+def solution(num, n):
+    answer = 0
+    
+    if num % n == 0:
+        
+        answer = 1
+        
+    else:
+        
+        answer = 0
+    
+    return answer
---
+++ programmers117.py
@@ -0,0 +1,17 @@
+# 2026.07.06
+# programmers117.py
+
+def solution(number, n, m):
+    
+    answer = 0
+    
+    if number % n == 0 and number % m == 0:
+        
+        answer = 1
+        
+    else:
+        
+        answer = 0
+    
+    
+    return answer
