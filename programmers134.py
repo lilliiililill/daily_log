9d7f986c6ab1dc969a4739c8e42ec18d80a8e23c
lilliiililill <asder0418@gmail.com>
2026-07-15 23:06:09 +0900

    Day 32 (4~5)

---
+++ programmers134.py
@@ -0,0 +1,21 @@
+# 2026.07.15
+# programmers134.py
+
+def solution(n):
+    
+    answer = []
+    answer.append(n)
+    
+    while n != 1:
+    
+        if n % 2 == 0:
+        
+            n //= 2
+            answer.append(n)
+        
+        else:
+        
+            n = 3 * n + 1
+            answer.append(n)
+    
+    return answer
