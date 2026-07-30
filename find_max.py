95a764ebef1a1153c3c9e54e4aa13201f4562bec
lilliiililill <asder0418@gmail.com>
2026-07-30 20:01:16 +0900

    find_max.py

---
+++ find_max.py
@@ -0,0 +1,15 @@
+# 2026.07.30
+# find_max.py
+
+numbers = [3, 7, 2, 9, 5]
+
+max_number = numbers[0]
+
+for number in numbers:
+
+    if number > max_number:
+
+        max_number = number
+
+
+print("가장 큰 수:", max_number)
