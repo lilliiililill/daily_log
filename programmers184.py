70a9bdf9a0757d9b123ff3b865c5dee96ed81ba4
lilliiililill <asder0418@gmail.com>
2026-08-19 19:58:38 +0900

    Day 42 (4~5)

---
+++ programmers184.py
@@ -0,0 +1,8 @@
+# 2026.08.19
+# programmers184.py
+
+def solution(my_string):
+    
+    answer = my_string.split()
+    
+    return answer
---
+++ programmers185.py
@@ -0,0 +1,6 @@
+# 2026.08.19
+# programmers185.py
+
+def solution(my_string):
+    answer = my_string.split()
+    return answer
