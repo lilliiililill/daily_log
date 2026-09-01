54d70928deb64817a2e362bc066cce7def1f9268
lilliiililill <asder0418@gmail.com>
2026-09-01 22:33:07 +0900

    Day 46 (4~5)

---
+++ programmers204.py
@@ -0,0 +1,10 @@
+# 2026.09.01
+# programmers204.py
+
+def solution(num_str):
+    
+    num_list = list(map(int, num_str))
+    
+    answer = sum(num_list)
+    
+    return answer
---
+++ programmers205.py
@@ -0,0 +1,6 @@
+# 2026.09.01
+# programmers205.py
+
+def solution(n_str):
+    answer = int(n_str)
+    return answer 
