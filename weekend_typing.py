35fabd6de541940300f9354d205d5606cf070403
lilliiililill <asder0418@gmail.com>
2026-09-12 03:44:43 +0900

    weekend_typing.py

---
+++ weekend_typing.py
@@ -0,0 +1,15 @@
+# 2026.09.12
+# weekend_typing.py
+
+numbers= [3, 7, 2, 9, 4]
+
+largest = numbers[0]
+
+for number in numbers:
+
+    if number > largest:
+
+        largest = number
+
+
+print("가장 큰 수:", largest)
