5f5b7ecac0529eca574b01a3b3fdba8410199570
lilliiililill <asder0418@gmail.com>
2026-09-14 20:48:42 +0900

    random_quote.py

---
+++ random_quote.py
@@ -0,0 +1,16 @@
+# 2026.09.14
+# random_quote.py
+
+import random
+
+quotes = [
+
+    "Small steps still move you forward.",
+    "Done is better than perfect.",
+    "Keep going. Future you will thank you.",
+    "Code, break, learn, repeat.",
+    "One commit is still progress."
+
+    ]
+
+print(random.choice(quotes))
