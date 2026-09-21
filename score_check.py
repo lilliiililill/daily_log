0014cba6dfde5bfb030a21d24c3f11f4414031ee
lilliiililill <asder0418@gmail.com>
2026-09-21 20:37:18 +0900

    score_check.py

---
+++ score_check.py
@@ -0,0 +1,15 @@
+# 2026.09.21
+# score_check.py
+
+scores = [72, 88, 91, 65, 79]
+
+for score in scores:
+
+    if score >= 80:
+
+        print(score, "PASS")
+
+
+    else:
+
+        print(score, "RETRY")
