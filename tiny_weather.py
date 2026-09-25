ae298364ef5a64d832cf37be18c782082ac33392
lilliiililill <asder0418@gmail.com>
2026-09-26 01:56:26 +0900

    tiny_weather.py

---
+++ tiny_weather.py
@@ -0,0 +1,17 @@
+# 2026.09.26
+# tiny_weather.py
+
+mood = "☁"
+
+
+if mood == "☀":
+
+    print("오늘은 좀 해볼 만하다.")
+
+elif mood == "☁":
+
+    print("잔디만 심고 가자.")
+
+else:
+
+    print("내일의 나에게 맡긴다.")
